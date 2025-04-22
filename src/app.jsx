a7004611d8c0678f445451cf83b644a9fbb7f2c9
Troy Presley <troy.presley@gmail.com>
2025-04-21 23:10:19 -1000

    update look to match design

--- src/app.jsx
+++ src/app.jsx
@@ -12,16 +12,16 @@ const APP = (_props, state) => {
 
 APP.initialState = {
   teams: [
-    { team: 'team1', success: undefined, error: undefined },
-    { team: 'team2', success: undefined, error: undefined },
-    { team: 'team3', success: undefined, error: undefined },
-    { team: 'team4', success: undefined, error: undefined },
-    { team: 'team5', success: undefined, error: undefined },
-    { team: 'team6', success: undefined, error: undefined },
-    { team: 'team7', success: undefined, error: undefined },
-    { team: 'team8', success: undefined, error: undefined },
-    { team: 'team9', success: undefined, error: undefined },
-    { team: 'team10', success: undefined, error: undefined },
+    { team: 'team1', label: 'STATION 1', success: undefined, error: undefined },
+    { team: 'team2', label: 'STATION 2', success: undefined, error: undefined },
+    { team: 'team3', label: 'STATION 3', success: undefined, error: undefined },
+    { team: 'team4', label: 'STATION 4', success: undefined, error: undefined },
+    { team: 'team5', label: 'STATION 5', success: undefined, error: undefined },
+    { team: 'team6', label: 'STATION 6', success: undefined, error: undefined },
+    { team: 'team7', label: 'STATION 7', success: undefined, error: undefined },
+    { team: 'team8', label: 'STATION 8', success: undefined, error: undefined },
+    { team: 'team9', label: 'STATION 9', success: undefined, error: undefined },
+    { team: 'team10', label: 'STATION 10', success: undefined, error: undefined },
   ],
   errorsWithoutTeam: []
 }
--- src/panel.jsx
+++ src/panel.jsx
@@ -1,15 +1,26 @@
 import { classes } from 'sygnal'
 
 const PANEL = (props, state) => {
-  const { team, type, success, error } = state
+  const { label, type, success, error } = state
 
-  const message = success ? type : (error || "waiting...")
+  var message
+  var done = false
+  if (success) {
+    message = type == 'logs' ? "logs received" : "🎉  SUCCESS 🎉 "
+    if (type !== "logs") {
+      done = true
+    }
+  } else {
+    message = error ? "⚠️ ERROR ⚠️" : "waiting..."
+  }
+  // const message = success ? type : (error || "waiting...")
 
-  const panelClasses = classes("panel", {success, error})
+  const panelClasses = classes("neon-text", "neon-box", {success: done, error})
 
   return (
     <div className={ panelClasses }>
-      { team } : { message }
+      <div className="label">{ label }</div>
+      <div className="message">{ message }</div>
     </div>
   )
 }
